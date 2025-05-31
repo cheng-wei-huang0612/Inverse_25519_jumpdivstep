44eae62da15c0d68488fa6543c2801fbe230c1ad
Cesare Huang <Cesarehuang@icloud.com>
2025-05-31 20:16:37 +0800

    optimized the before loop part, bench

--- src/i_loop/slothy_driver.py
+++ src/i_loop/slothy_driver.py
@@ -16,23 +16,23 @@ slothy = Slothy(arch, target)
 
 
 # example
-slothy.load_source_from_file("i_loop_clean.S")
+slothy.load_source_from_file("before_loop_clean.S")
 slothy.config.variable_size = True
 slothy.config.constraints.stalls_first_attempt = 512
 
 
-slothy.config.inputs_are_outputs = True
-# slothy.config.outputs = ["x6","x7","x8","x9","x10","x11","x12","x13",
-#                          "v0","v1","v2","v3","v4","v5","v6","v7","v8","v9","v10",
-#                          "v11","v12","v13","v14","v15",
-#                          ]
+#slothy.config.inputs_are_outputs = True
+slothy.config.outputs = ["x6","x7","x8","x9","x10","x11","x12","x13",
+                         "v0","v1","v2","v3","v4","v5","v6","v7","v8","v9","v10",
+                         "v11","v12","v13","v14","v15","v16","v18","sp"
+                         ]
 slothy.config.reserved_regs = ["x0","x1","x2","x3","x4","x5","x6",
                                 "x24","x25","x26","x27","x28",
-                               "x29","x30","sp"]
+                               "x29","x30"]
 slothy.config.reserved_regs_are_locked = True
 
 
 
 
 slothy.optimize()
-slothy.write_source_to_file("i_loop_opt_a72.s")
+slothy.write_source_to_file("before_loop_opt_a72.s")
