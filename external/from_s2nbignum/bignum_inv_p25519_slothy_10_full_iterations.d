040a6239ce22c53473dd5797aeeb6bb76325dada
Cesare Huang <Cesarehuang@icloud.com>
2025-07-20 15:51:47 +0800

    benchmark

---
+++ external/from_s2nbignum/bignum_inv_p25519_slothy_10_full_iterations.d
@@ -0,0 +1,3 @@
+../external/from_s2nbignum/bignum_inv_p25519_slothy_10_full_iterations.o: \
+ ../external/from_s2nbignum/bignum_inv_p25519_slothy_10_full_iterations.S \
+ ../external/from_s2nbignum/_internal_s2n_bignum.h
